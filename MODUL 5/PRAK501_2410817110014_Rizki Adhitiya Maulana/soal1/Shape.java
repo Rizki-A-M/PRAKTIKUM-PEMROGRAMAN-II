4795f242fdffa3bf9e687122453e5015c9fbdead
Rizki-A-M <rizkiadhitiyamaulana@gmail.com>
2025-11-05 20:56:41 +0700

    Add files via upload

---
+++ MODUL 5/PRAK501_2410817110014_Rizki Adhitiya Maulana/soal1/Shape.java
@@ -0,0 +1,18 @@
+package soal1;
+
+public abstract class Shape
+{
+    protected String shapeName;
+
+    public Shape(String name)
+    {
+        shapeName = name;
+    }
+
+    public abstract double area();
+
+    public String toString()
+    {
+        return shapeName;
+    }
+}
